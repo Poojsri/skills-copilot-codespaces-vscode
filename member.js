e36ebe18f9fba399b90cce30b8ec9dcc10451985
Pooja Srikanth <121930344+Poojsri@users.noreply.github.com>
2024-12-18 05:29:46 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,13 @@
+function skillsMember(){
+    var member = document.getElementById('member');
+    var skill = document.getElementById('skill');
+    var experience = document.getElementById('experience');
+    var education = document.getElementById('education');
+    var project = document.getElementById('project');
+
+    member.style.display = "block";
+    skill.style.display = "none";
+    experience.style.display = "none";
+    education.style.display = "none";
+    project.style.display = "none";
+}
